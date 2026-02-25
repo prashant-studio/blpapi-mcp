4fcd083ab245de9f1d6a8fcb6a021f02a1c9008d
Prashant Kumar <prashant@studiomgmt.co>
2026-02-25 17:10:54 +0530

    fix kwargs

--- src/blpapi_mcp/blp_mcp_server.py
+++ src/blpapi_mcp/blp_mcp_server.py
@@ -36,6 +36,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg reference data"
   )
   async def bdp(tickers:typing.List[str], flds:typing.List[str], kwargs: types.BloombergKWArgs = None) -> typing.Any:
+    if kwargs is None:
+      return blp.bdp(tickers=tickers, flds=flds)
     return blp.bdp(tickers=tickers, flds=flds, kwargs=kwargs)
 
   @mcp.tool(
@@ -43,6 +45,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg block data"
   )
   async def bds(tickers:typing.List[str], flds:typing.List[str], use_port:bool=False, kwargs:types.BloombergKWArgs=None) -> typing.Any:
+    if kwargs is None:
+      return blp.bds(tickers=tickers, flds=flds, use_port=use_port)
     return blp.bds(tickers=tickers, flds=flds, use_port=use_port, kwargs=kwargs)
 
   @mcp.tool(
@@ -50,6 +54,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg historical data"
   )
   async def bdh(tickers:typing.List[str], flds:typing.List[str], start_date:typing.Union[None, str]=None, end_date:str="today", adjust: typing.Union[str, None] = None, kwargs: types.BloombergKWArgs = None) -> typing.Any:
+    if kwargs is None:
+      return blp.bdh(tickers=tickers, flds=flds, start_date=start_date, end_date=end_date, adjust=adjust)
     return blp.bdh(tickers=tickers, flds=flds, start_date=start_date, end_date=end_date, adjust=adjust, kwargs=kwargs)
 
   @mcp.tool(
@@ -57,6 +63,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg intraday bar data"
   )
   async def bdib(ticker:str, dt:str, session:str = "allday", typ:str = "TRADE", kwargs: types.BloombergKWArgs=None) -> typing.Any:
+    if kwargs is None:
+      return blp.bdib(ticker=ticker, dt=dt, session=session, typ=typ)
     return blp.bdib(ticker=ticker, dt=dt, session=session, typ=typ, kwargs=kwargs)
 
   @mcp.tool(
@@ -64,6 +72,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg tick data"
   )
   async def bdtick(ticker:str, dt:str, session:str="allday", time_range:typing.Union[None, typing.Tuple[str]]=None, types:typing.Union[None, typing.List[str]]=None, kwargs:types.BloombergKWArgs=None) -> typing.Any:
+    if kwargs is None:
+      return blp.bdtick(ticker=ticker, dt=dt, session=session, time_range=time_range, types=types)
     return blp.bdtick(ticker=ticker, dt=dt, session=session, time_range=time_range, types=types, kwargs=kwargs)
 
   @mcp.tool(
@@ -71,6 +81,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg earning exposure by Geo or Products"
   )
   async def earning(ticker:str, by:str="Geo", typ:str="Revenue", ccy:typing.Union[None,str]=None, level: typing.Union[None, str]=None, kwargs:types.BloombergKWArgs=None) -> typing.Any:
+    if kwargs is None:
+      return blp.earning(ticker=ticker, by=by, typ=typ, ccy=ccy, level=level)
     return blp.earning(ticker=ticker, by=by, typ=typ, ccy=ccy, level=level, kwargs=kwargs)
 
   @mcp.tool(
@@ -78,6 +90,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg divident / split history"
   )
   async def dividend(tickers:typing.List[str], typ:str="all", start_date:typing.Union[None,str]=None, end_date:typing.Union[None,str]=None, kwargs:types.BloombergKWArgs=None) -> typing.Any:
+    if kwargs is None:
+      return blp.dividend(tickers=tickers, typ=typ, start_date=start_date, end_date=end_date)
     return blp.dividend(tickers=tickers, typ=typ, start_date=start_date, end_date=end_date, kwargs=kwargs)
 
   @mcp.tool(
@@ -85,6 +99,8 @@ def serve(args: types.StartupArgs):
     description="Get Bloomberg equity screening"
   )
   async def beqs(screen:str, asof:typing.Union[None,str]=None, typ:str="PRIVATE", group:str="General", kwargs:types.BloombergKWArgs=None) -> typing.Any:
+    if kwargs is None:
+      return blp.beqs(screen=screen, asof=asof, typ=typ, group=group)
     return blp.beqs(screen=screen, asof=asof, typ=typ, group=group, kwargs=kwargs)
 
   @mcp.tool(
